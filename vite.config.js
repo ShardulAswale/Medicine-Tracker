f23744a98c10569746a0898e0408fbe5e51ce67a
ShardulAswale <unrealdoc12@gmail.com>
2023-09-03 12:38:37 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    server: {
+      watch: {
+        ignored: ["!**/node_modules/autopreview/**"],
+      },
+    },
+    // The watched package must be excluded from optimization,
+    // so that it can appear in the dependency graph and trigger hot reload.
+    optimizeDeps: {
+      exclude: ["autopreview"],
+    },
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Medicine-Tracker/'
+  }
+
+  return config
 })
